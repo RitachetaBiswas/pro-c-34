d0af3a719ebe48f28b1f3ac604498e3772f59d4c
RitachetaBiswas <74050249+RitachetaBiswas@users.noreply.github.com>
2021-02-28 20:06:45 +0530

    Add files via upload

---
+++ VirtualPet-Template-main/VirtualPet-Template-main/sketch.js
@@ -0,0 +1,84 @@
+var dog,dogImg,dogImg1;
+var database;
+var foodS,foodStock;
+var fedTime, lastFed;
+var feed, addFood;
+var foodObj
+
+function preload(){
+   dogImg=loadImage("Images/Dog.png");
+   dogImg1=loadImage("Images/happy dog.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1000, 400);
+  foodObj = new Food();
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  feed = createButton("feed the dog");
+  feed.position(700, 95);
+  feed.mousePressed(feedDog);
+  addFood = createButton("Add Food");
+  addFood.position(800, 95);
+  addFood.mousePressed(addFoods);
+}
+
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+ foodObj.display();
+ fedTime=database.ref('feedTime');
+ fedTime.on("value", function(data){
+   lastFed= data.val();
+ })
+
+ fill(255,255,254);
+ textSize(15);
+ if(lastFed >= 12) {
+   text("last feed"+lastFed%12+"PM", 350, 30);
+ } else if(lastFed == 0) {
+  text("last feed 12 AM", 350, 30);
+} else {
+  text("last feed"+lastFed+"AM", 350, 30);
+}
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+//function to update food stock and last fed time
+function feedDog(){
+  dog.addImage(dogImg1);
+  
+  if(foodObj.getFoodStock()<= 0){
+    foodObj.updateFoodStock(foodObj.getFoodStock()*0);
+  }else{
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  }
+  
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+  })
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
